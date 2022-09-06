1f8831b1ef6779948910f4485fcf6a739c95e021
Anita Jose <anitajose119@gmail.com>
2022-09-06 00:28:09 -0400

    Add Heroku

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,48 +1,61 @@
 const path = require('path')
 const fs = require('fs')
+const {promisify} = require('util')
 // npm package to create unique ids
 const uniqid = require('uniqid')
+const { del } = require('express/lib/application')
 
 const router = require('express').Router()
-
-// let db = fs.readFileSync('db/db.json')
-// db = JSON.parse(db)
-let db = JSON.parse(fs.readFileSync('db/db.json'))
+const readFilePromise = promisify(fs.readFile)
+const writeFilePromise = promisify(fs.writeFile)
 
 // read the db.json file and return saved notes as json
-router.get('api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../db/db.json'))
-    res.json(db)
+router.get('/notes', (req, res) => {
+    readDb().then(value => {
+        res.json(JSON.parse(value));
+    }).catch(err => {
+        console.log(err);
+    })
 })
 
 // receive a new note, add it to db.json, and return it to the client
-router.post('api/notes', (req, res) => {
+router.post('/notes', (req, res) => {
     console.log("Req body:", req.body)
-    res.json(db)
-    // // create notes body
+    readDb().then(value => {
+        const notes = JSON.parse(value);
+        // // create notes body
     let createNote = {
         title: req.body.title,
         text: req.body.text,
         // unique id for the note
         id: uniqid()
     }
-    db.push(createNote)
-    updateDb()
-    return console.log("New note added: " + createNote.title)
+    notes.push(createNote)
+    updateDb(JSON.stringify(notes)).then(() => {
+        res.sendStatus(200)
+    })
+    })
 })
 
 // delete a note by using its id
-router.delete('api/notes/:id', (req, res) => {
-    db.splice(req.params.id, 1)
-    updateDb()
-    console.log("Deleted note with id: " + req.params.id);
+router.delete('/notes/:id', (req, res) => {
+    readDb().then(value => {
+        const notes = JSON.parse(value)
+    let deleteNotes = notes.filter(item => item.id !== req.params.id)
+    // updateDb()
+    // console.log("Deleted note with id: " + req.params.id);
+    updateDb(JSON.stringify(deleteNotes)).then(() => {
+        res.sendStatus(200)
+    })
+    })
 })
 
-function updateDb() {
-    fs.writeFile("db/db.json",json.stringify(db, '\t'),err => {
-        if(err) throw err
-        return true
-    })
+function updateDb(data) {
+    return writeFilePromise(path.join(__dirname, '../db/db.json'), data)
+}
+
+function readDb() {
+    return readFilePromise(path.join(__dirname, '../db/db.json')) 
 }
 
 module.exports = router
